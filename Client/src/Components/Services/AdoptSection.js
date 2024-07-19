e303db175abc559b34ec4a28b4052fc54f45d882
Saloni Bisen <saloni.bisen411@gmail.com>
2024-07-19 20:57:26 +0530

    changes

--- Client/src/Components/Services/AdoptSection.js
+++ Client/src/Components/Services/AdoptSection.js
@@ -19,20 +19,20 @@ const AdoptSection = () => {
 
       <h3>Benefits of Pet Adoption</h3>
       <ul>
-        <li>Provide a loving home to a pet in need</li>
-        <li>Experience the unconditional love of a pet</li>
-        <li>Create lasting memories and cherished moments</li>
+        <li style={{ fontSize: '13px' }}>Provide a loving home to a pet in need</li>
+        <li style={{ fontSize: '13px' }}>Experience the unconditional love of a pet</li>
+        <li style={{ fontSize: '12.8px' }}>Create lasting memories and cherished moments</li>
       </ul>
 
       <h3>Adoption Process</h3>
       <ol>
-        <li>Fill out an adoption application</li>
-        <li>Meet potential pets in person</li>
-        <li>Complete the necessary paperwork</li>
+        <li style={{ fontSize: '13px' }}>Fill out an adoption application</li>
+        <li style={{ fontSize: '13px' }}>Meet potential pets in person</li>
+        <li style={{ fontSize: '13px' }}>Complete the necessary paperwork</li>
       </ol>
 
       <h3>Responsibilities</h3>
-      <p>
+      <p style={{ fontSize: '12.5px' }}>
         Adopting a pet comes with responsibilities, including feeding, grooming,
         regular exercise, and providing medical care.
       </p>
