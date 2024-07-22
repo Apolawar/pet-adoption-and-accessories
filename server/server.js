03c732e9084368289ee8ac1a74d54dfaec365abe
Apolawar <polawaranant1@gmail.com>
2024-07-22 21:59:32 +0530

    vercel-delete

--- server/server.js
+++ server/server.js
@@ -1,36 +1,30 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const petRouter = require('./Routes/PetRoute');
-const AdoptFormRoute = require('./Routes/AdoptFormRoute');
-const AdminRoute = require('./Routes/AdminRoute');
-const userRoute = require('./Routes/userRoute');
-const cors = require('cors');
-const path = require('path');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const petRouter = require("./Routes/PetRoute");
+const AdoptFormRoute = require("./Routes/AdoptFormRoute");
+const AdminRoute = require("./Routes/AdminRoute");
+const userRoute = require("./Routes/userRoute");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
-app.use(cors(
-  {
-    origin: ["https://pet-adoption-and-accessories.vercel.app/"],
-    methods: ["POST", "GET"],
-    credentials: true
-  }
-));
+app.use(cors());
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(petRouter);
-app.use('/form', AdoptFormRoute);
-app.use('/admin', AdminRoute);
-app.use('/user', userRoute);
+app.use("/form", AdoptFormRoute);
+app.use("/admin", AdminRoute);
+app.use("/user", userRoute);
 
 mongoose
-  .connect("mongodb+srv://polawaranant1:s8yzmiZuaO4UNo5u@mycluster.t6paxsz.mongodb.net/MyCluster?retryWrites=true&w=majority&appName=MyCluster", {
+  .connect("mongodb://localhost:27017/pet", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Connected to DB");
